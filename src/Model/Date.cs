14b7d5f82d12f9814caceb46c1c9b3967a216e2c
Chester Southwood <csouthwood@eagles.ewu.edu>
2019-01-21 22:08:32 -0800

    Files_From_Site

---
+++ src/Model/Date.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NetCoreBiginner
+{
+    public class Date
+    {
+        [Required]
+        [Range(1, 31)]
+        public int Day { get; set; }
+
+        [Required]
+        [Range(1, 12)]
+        public int Month { get; set; }
+
+        private int ThisYear { get; set; } = DateTime.Now.Year;
+
+        [Required]
+        [Range(1900, 2099)]
+        public int Year { get; set; }
+
+        public static string GetDay()
+        {
+            int intDay = (int)DateTime.Now.DayOfWeek;
+            intDay = intDay % 7;
+            string day;
+            switch (intDay)
+            {
+                case 0:
+                    day = "Sunday";
+                    break;
+                case 1:
+                    day = "Monday";
+                    break;
+                case 2:
+                    day = "Tuesday";
+                    break;
+                case 3:
+                    day = "Wednesday";
+                    break;
+                case 4:
+                    day = "Thursday";
+                    break;
+                case 5:
+                    day = "Friday";
+                    break;
+                default:
+                    day = "Saturday";
+                    break;
+            }
+            return day;
+        }
+
+        public static string GetMonth()
+        {
+            int intMonth = DateTime.Now.Month;
+            string strMonth;
+            switch (intMonth)
+            {
+                case 0:
+                    strMonth = "January";
+                    break;
+                case 1:
+                    strMonth = "Februrary";
+                    break;
+                case 2:
+                    strMonth = "March";
+                    break;
+                case 3:
+                    strMonth = "April";
+                    break;
+                case 4:
+                    strMonth = "May";
+                    break;
+                case 5:
+                    strMonth = "June";
+                    break;
+                case 6:
+                    strMonth = "July";
+                    break;
+                case 7:
+                    strMonth = "August";
+                    break;
+                case 8:
+                    strMonth = "September";
+                    break;
+                case 9:
+                    strMonth = "October";
+                    break;
+                case 10:
+                    strMonth = "November";
+                    break;
+                default:
+                    strMonth = "December";
+                    break;
+            }
+            return strMonth;
+        }
+
+    }
+}
---
+++ src/Page/GraduationCalculation.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NetCoreBiginner.Pages
+{
+    public class GraduationCalculationModel : PageModel
+    {
+        [TempData]
+        public string Message { get; set; }
+
+        [BindProperty]
+        public Date Date1 { get; set; }
+
+        [BindProperty]
+        public Date Date2 { get; set; }
+
+        public Date DateResult { get; set; } = new Date();
+
+        public void onGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostCalculateAsync()
+        {
+
+            if (!ModelState.IsValid)
+            {
+                Message = $"Cannot Calculate Age From Current Data!";
+                return Page();
+            }
+
+            DateResult.Day = Date1.Day - Date2.Day;
+            DateResult.Month = Date1.Month - Date2.Month;
+            DateResult.Year = Date1.Year - Date2.Year;
+            DateTime dateTime1 = new DateTime(Date1.Year, Date1.Month, Date1.Day);
+            DateTime dateTime2 = new DateTime(Date2.Year, Date2.Month, Date2.Day);
+            TimeSpan timeResult = dateTime2.Subtract(dateTime1);
+            Message = $"You'll Be {timeResult.Days / 365} When You Graduate!!!!";
+            return RedirectToPage("./", );
+        }
+    }
+}
